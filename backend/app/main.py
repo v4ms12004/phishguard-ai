44456970a5890b8a00d816c72c6bca6bc31a8159
Vamsi <115450889+v4ms12004@users.noreply.github.com>
2025-11-23 13:12:13 -0600

    Updated CORS middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,9 +16,9 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"], 
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["*"],  
     allow_headers=["*"],
 )
 
